068643720c70d1a0a5650ac7bba000f07d77dc80
provsalt <gooraymond39@gmail.com>
2021-12-14 00:20:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wagmi-autostake
+This projects helps people using OHM forks like wagmi auto stake their bonds every few hours just before rebase. I decided to make one for euphoria since I'm pretty active in the harmony network.
+
+### Instructions
+1. Clone the repository
+2. npm install / yarn install
+3. Create an env file and add your private key with this format
+```env
+PRIVATE_KEY=YOUR_KEY_HERE
+```
+4. Run npx run scripts/autostake_wagmi.ts
+5. Leave it on and enjoy
+
+### Screenshots
+![Demo 1]("./images/demo1.png")
+![Demo 2]("./images/demo2.png")
---
+++ scripts/autostake_wagmi.ts
@@ -0,0 +1,61 @@
+import { ethers } from "hardhat";
+import { Logger } from "tslog";
+const addresses = require("../addresses.json");
+
+const log: Logger = new Logger({
+  displayFunctionName: false,
+  displayFilePath: "hidden",
+});
+let redeemed = false;
+
+async function main() {
+  // Retrieve accounts from the local node
+  const accounts = await ethers.provider.listAccounts();
+  console.log(accounts);
+  const StakingDistributorAddress = addresses.StakingDistributor;
+  const StakingDistributor = await ethers.getContractFactory(
+    "StakingDistributor"
+  );
+  const stakingDistributor = await StakingDistributor.attach(
+    StakingDistributorAddress
+  );
+  // Check every 20 second or RIP CPU
+  setInterval(async () => {
+    const epoch = await stakingDistributor.nextEpochTime();
+    const time = new Date().valueOf() / 1000;
+    // This is abit inefficient however it's 12am and I'm braindead atm
+    if (time > epoch - 60) {
+      log.info("Rebasing soon, redeeming bonds.");
+      if (!redeemed) {
+        redeem(accounts[0]);
+      }
+      redeemed = true;
+    } else {
+      log.debug(
+        "Rebasing in " + ((epoch - time) / 60 / 60).toFixed(1) + " hours."
+      );
+    }
+  }, 20000);
+}
+
+async function redeem(account: string) {
+  for (let bond in addresses.bonds) {
+    log.info("Redeeming bond " + bond);
+    const BondAddress = addresses.bonds[bond];
+    const BondDepository = await ethers.getContractFactory("BondDepository");
+    const bondDepository = await BondDepository.attach(BondAddress);
+    const bondBalance = await bondDepository.pendingPayoutFor(account);
+    if (bondBalance > 1) {
+      // the boolean argument is whether to auto stake for the user.
+      await bondDepository.redeem(account, true);
+      log.info("Redeemed " + bondBalance + " for " + bond);
+    }
+  }
+}
+
+main()
+  // .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
