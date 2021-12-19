33274e38f07516e01e70ec83aa17ccf30263838f
provsalt <gooraymond39@gmail.com>
2021-12-19 16:07:30 +0800

    Add minimum amount

--- scripts/redeem.py
+++ scripts/redeem.py
@@ -1,6 +1,4 @@
-from brownie import Contract, config, network, interface
-from brownie.network import account
-
+from brownie import config, network, interface
 from scripts.utils import get_account
 from loguru import logger
 
@@ -8,7 +6,7 @@ def redeem():
     bonds = config["networks"][network.show_active()]["bonds"]
     for bond, address in bonds.items():
         balance = interface.IBond(address).pendingPayoutFor(get_account())
-        if balance < 1000:
+        if balance < (config["minimum_wagmi"] * 10 ** 9):
             continue
         formatted = f"{balance / 10 ** 9:.9f}"
         logger.info(f"Redeeming {formatted} WAGMI from {bond}")
